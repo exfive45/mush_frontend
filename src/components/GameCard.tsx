2bd2dbff60fddec85751a670f46472e32cb74fd2
Mbidi-store-project <exfive35@gmail.com>
2025-10-29 17:14:17 +0100

    get optimize images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
 import type { Game } from "../hooks/useGame"
 import { PlatformIconList } from "./PlatformIconList"
 import CreditScore from "./CreditScore"
+import getCropedImageUrl from "../services/image_url"
 
 interface Props {
     game:Game
@@ -13,7 +14,7 @@ const GameCard = ({game}:Props) => {
     <>
     
 <Card borderRadius={10} overflow='hidden'>
-    <Image src={game.background_image}/>
+    <Image src={getCropedImageUrl(game.background_image)}/>
     <CardBody>
     <Heading fontSize="xl">{game.name}</Heading>
 
